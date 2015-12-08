33f4c9124896fb66dcd37d9403cd44d1b2a5395d
Islam-Mamayeu <Islam_Mamayeu@epam.com>
2015-12-08 17:24:26 +0300

    First commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Logic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double prec = 0;
+            double number = 0;
+            int n = 0;
+            bool isInt;
+            Console.WriteLine("Enter number:");
+            isInt = double.TryParse(Console.ReadLine(), out number);
+            Console.WriteLine("Enter root:");
+            isInt = Int32.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine("Enter precision:");
+            isInt = double.TryParse(Console.ReadLine(), out prec);
+            NutonClass.Root(number, n, prec);
+
+
+        }
+    }
+}
---
+++ Logic/NutonClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+   public class NutonClass
+    {
+       public static void Root(double number, int n, double prec)
+        {
+
+            double A1 = number/2;
+            double result =0;
+            double precResult=1;
+            while (Math.Abs(precResult) > prec)
+            {
+                result = ((n-1)*A1 + (number / Math.Pow(A1,n-1)))/n;
+                precResult = A1-result;
+                A1 = result;
+
+
+            }
+            Console.WriteLine(result);
+            Console.WriteLine( Math.Pow(number, 1.0/n));
+            Console.ReadKey();
+
+        }
+    }
+}
